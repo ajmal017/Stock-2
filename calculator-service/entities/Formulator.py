924c288a442e576c04a975f6171c74a41fc68f04
ignacio <ignaciovidal89@gmail.com>
2020-03-21 23:10:56 +0000

    expanding efficient frontier to calculate sharpe ratio

--- calculator-service/entities/Formulator.py
+++ calculator-service/entities/Formulator.py
@@ -120,10 +120,54 @@ class EfficientFrontier(FormulatorAbstract):
                 'volatility': round(np.sqrt(np.dot(weights.T, np.dot(log_returns.cov() * 250, weights))) * 100, 4)
             }
             frontier.append(result)
-
         return frontier
 
 
 class EfficientFrontierFactory(AbstractFactory):
     def factory(self):
         return EfficientFrontier()
+
+
+
+
+class EfficientFrontierSharpe(FormulatorAbstract):
+    def calculate(self, df):
+        # calculate daily and annual returns of the stocks
+        returns_daily = df.pct_change()
+        returns_annual = returns_daily.mean() * 250
+
+        # get daily and covariance of returns of the stock
+        cov_daily = returns_daily.cov()
+        cov_annual = cov_daily * 250
+
+        # empty lists to store returns, volatility and weights of imiginary portfolios
+
+        result = []
+
+        # set the number of combinations for imaginary portfolios
+        num_assets =,df.shape[0]
+        num_portfolios = 1000
+
+        # set random seed for reproduction's sake
+        np.random.seed(101)
+
+        for single_portfolio in range(num_portfolios):
+            weights = np.random.random(num_assets)
+            weights /= np.sum(weights)
+            returns = np.dot(weights, returns_annual)
+            volatility = np.sqrt(np.dot(weights.T, np.dot(cov_annual, weights)))
+            sharpe = returns / volatility
+
+            # create dictionary with calculations and push into results array
+            portfolio = {'returns': returns,
+                         'volatility': volatility,
+                         'sharpe_ratio': sharpe,
+                         'weights': weights
+                         }
+            result.append(portfolio)
+            return portfolio
+
+
+class EfficientFrontierSharpeFactory(AbstractFactory):
+    def factory(self):
+        return EfficientFrontierSharpe()
