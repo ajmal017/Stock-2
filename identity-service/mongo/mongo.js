ff66ba3c266c19b47b03bcfff5c38f240dbdbdcb
ignacio <ignaciovidal89@gmail.com>
2020-05-03 19:11:16 +0100

    Identity - NODE_ENV added to mongo driver

--- identity-service/mongo/mongo.js
+++ identity-service/mongo/mongo.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const CONSUMER_SERVICE = 'IDENTITY'
+const NODE_ENV = process.env.NODE_ENV
 const MONGO_URL = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0-0hmcf.mongodb.net/${CONSUMER_SERVICE}-${NODE_ENV}?retryWrites=true&w=majority`
 
 const connectDB = async () => {
